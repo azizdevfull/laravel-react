f34945b40adb2c19dd1a1431c068dc017cef6a2e
azizdevfull <aziz16110904@gmail.com>
2022-12-17 18:45:42 +0500

    completed react log out

--- reactecom/src/layouts/frontend/Navbar.js
+++ reactecom/src/layouts/frontend/Navbar.js
@@ -3,13 +3,10 @@ import {Link, useHistory} from 'react-router-dom';
 
 import swal from 'sweetalert';
 import axios from 'axios';
-// import { Link } from 'react-router-dom';
 
+function Navbar() {
 
-
-function Navbar(){
-
-  const history = useHistory();
+    const history = useHistory();
     const logoutSubmit = (e) => {
         e.preventDefault();
         
@@ -18,8 +15,12 @@ function Navbar(){
             {
                 localStorage.removeItem('auth_token');
                 localStorage.removeItem('auth_name');
-                swal("Success",res.data.message,"success");
+                const aziz = swal("Success",res.data.messages,"success");
                 history.push('/');
+
+                if (aziz) {
+                  window.location.reload();
+                }
             }
         });
 
@@ -31,10 +32,10 @@ function Navbar(){
         AuthButtons = (
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/login">Login</Link>
+                    <a className="nav-link" href="/login">Login</a>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/register">Register</Link>
+                    <a className="nav-link" href="/register">Register</a>
                 </li>
             </ul>
         );
@@ -48,27 +49,26 @@ function Navbar(){
         );
     }
 
-return (
-<nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow sticky-top">
-  <div className="container">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Collection</a>
-        </li>
-        {AuthButtons}
-      </ul>
-    </div>
-  </div>
-</nav>
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary shadow sticky-top">
+            <div className="container">
+
+                <Link className="navbar-brand" to="#">Ecom Project</Link>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/">Home</Link>
+                        </li>
+                        {AuthButtons}
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
